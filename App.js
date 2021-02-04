9e42d4dee532baf58a6f2bb3f339fd6d45e75074
vishwa96vishu <smvishwadharmaratna96@gmail.com>
2021-02-04 21:44:03 +0530

    testfirstcommit

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default  function App() {
   
   return (
     <View style={styles.container}>
       
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start gfd working on your app!</Text>
       
       
       <StatusBar style="auto" />
